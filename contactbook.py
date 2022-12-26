ac529f9e89bc1a01bc2e22ea1fe3f7f83cb7cfbe
tomkara95 <thomaskarampidis1@gmail.com>
2022-12-26 23:11:15 +0200

    contact book created

---
+++ contactbook.py
@@ -0,0 +1,127 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(user='root', password='admin',
+                              host='127.0.0.1',
+                              database='contacts')
+
+mycursor = mydb.cursor()
+
+
+
+
+exitflag = False
+choiceflag = True
+while exitflag == False:
+    while choiceflag==True:
+        print("MENU")
+        print("1.New Entry")
+        print("2.Call Entry")
+        print("3.Modify Entry")
+        print("4.Delete Entry")
+        choice = int (input("Select 1 2 3 or 4: "))
+        if (choice == 1 or choice == 2 or choice == 3 or choice == 4):
+            choiceflag=False 
+        else:
+            print("Not accepted choice, ")    
+        
+
+
+        if choice == 1:
+            print("New contact : ")
+            x = input("LastName : ")
+            y = input("FirstName : ")
+            sqlentry = "SELECT * FROM contactsbook WHERE Lastname = %s and Firstname = %s"
+            dr = (x,y, )
+            mycursor.execute(sqlentry,dr)
+            myresult = mycursor.fetchall()
+            if myresult:
+                print('already exists')
+            else:
+                z = input("PhoneNumber : ")
+                c = input("Email : ")
+                sqlentry1 = "INSERT INTO contactsbook(Lastname,Firstname,Phonenumber,Email) VALUES (%s , %s, %s, %s)"
+                val = (x,y,z,c)
+                mycursor.execute(sqlentry1,val)
+                mydb.commit()
+                print("new entry made successfully!")
+
+        
+        
+        if choice == 2:
+            flag2 = True
+            while flag2== True:
+                user_input = input ("Contact search Lastname : ")
+                user_input2 = input ("Contact's Firstname: ")
+                sql = "SELECT * FROM contactsbook WHERE Lastname = %s and Firstname = %s "
+                old = (user_input,user_input2, )
+                mycursor.execute(sql,old)
+                myresult2 = mycursor.fetchall()
+                if not myresult2:
+                    print ('It does not exist ')
+                for i in myresult2:
+                    print(i)
+                flag2 = False
+
+        if choice ==3:
+            print('give an existing account: ')
+            x = input("Lastname: ")
+            y = input("Firstname: ")
+            sql = "SELECT * FROM contactsbook WHERE Lastname = %s and Firstname= %s"
+            adr = (x,y, )
+
+            mycursor.execute(sql, adr)
+
+            myresult3 = mycursor.fetchall()
+
+            if myresult3:
+                z = input("NewPhonenumber: ")
+                sql = "UPDATE contactsbook SET Phonenumber = %s  WHERE Lastname = %s and Firstname= %s "
+                val = (z,x,y)
+                mycursor.execute(sql, val)
+                mydb.commit()
+                print("Phone changed! ")
+                c = input("New Email: ")
+                sql1 = "UPDATE contactsbook SET Email = %s  WHERE Lastname = %s and Firstname= %s "
+                val2 = (c,x,y)
+                mycursor.execute(sql1, val2)
+                mydb.commit()
+                print("Email changed! ")
+            else:
+                print('Account does not exist! ')
+            
+
+        if choice ==4:
+            print("Delete contact : ")
+            x = input("LastName : ")
+            y = input("FirstName : ")
+            sqlentry = "SELECT * FROM contactsbook WHERE Lastname = %s and Firstname = %s"
+            dr = (x,y, )
+            mycursor.execute(sqlentry,dr)
+            myresult4 = mycursor.fetchall()
+            if myresult4:
+                print("contact exists")
+                sqldel = "DELETE FROM contactsbook WHERE Lastname = %s and Firstname = %s "
+                de = (x,y)
+                mycursor.execute(sqldel,de)
+                mydb.commit()
+                print("Contact Deleted! ")
+
+            else:
+                print("Contact Not Found!")
+
+            
+
+    exittemp= True
+    while exittemp==True:
+        exit = input("exit? y/n _")
+        if exit == "y" or exit=="Y":
+            exittemp = False
+            exitflag = True
+            choiceflag= False
+        elif exit == "n" or exit == "N":
+            exittemp = False
+            #exitflag == False
+            choiceflag = True
+        else:
+            print("not accepted answer,try again : ")
---
+++ contactsql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contactsbook (
+	Lastname varchar(255),
+    Firstname varchar(255),
+    Phonenumber int(10),
+    Email varchar(255)
+);
+
+
